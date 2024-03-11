6887a119e157e7e1a56c1911acb1666ac87b4dec
Matthew Kirk <machukirk@gmail.com>
2024-03-11 11:22:59 +0000

    Fixed issue where modules did not work on Cordova

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,35 +1,7 @@
+import connectivity from "./connectivity.js"
 
 document.addEventListener('deviceready', onDeviceReady, false)
 
-let iab
-let onlineUrl
-
 function onDeviceReady() {
-    iab = cordova.InAppBrowser;
-    
-    checkConnection()
-    
-    document.addEventListener("offline", setOffline, false);
-    document.addEventListener("online", setOnline, false);
-    
+    connectivity()
 }
-
-function checkConnection() {
-    var networkState = navigator.connection.type
-
-    if ( networkState === 'none' ) {
-        setOffline()
-    } else {
-        setOnline()
-    }
-}
-
-function setOffline () {
-    if ( ! onlineUrl ) return
-    onlineUrl.close()
-}
-
-function setOnline () {
-    onlineUrl = iab.open('https://almanac.digital', '_self', 'location=no,clearsessioncache=yes,zoom=no')
-}
-
